e12bd497ae567e3fc663d1b5e9abed3fd37d373f
Emmanuel <emmanueltar14@gmail.com>
2022-08-17 13:04:16 +0100

    increment count and display the additon

--- index.js
+++ index.js
@@ -3,8 +3,29 @@
 //increment count on clicked button
 //
 
-function incrementCount() {
- // count++;
- console.log("Button Clicked")
- //render(); 
+// function incrementCount() {
+//  // count++;
+//  console.log("Button Clicked")
+//  //render(); 
+// }
+
+// function callout() {
+//   console.log(42)
+// }
+
+// callout();
+
+// let lap1 = 34;
+// let lap2 = 42;
+// let lap3 = 50;
+// function averageLapTime() {
+//   let sum = (lap1 + lap2 + lap3);
+//   console.log(sum);
+// }
+// averageLapTime();
+
+let count = 0;
+function increment() {
+  count = count + 1;
+  console.log(count);
 }
