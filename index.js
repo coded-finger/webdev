c67fa30291d84d3a8db8d25891eeb68f6090c7cf
Emmanuel <emmanueltar14@gmail.com>
2022-08-18 13:50:40 +0100

    save countinto the system

--- index.js
+++ index.js
@@ -1,31 +1,13 @@
-// let initialize count = 0;
-//listen to click on increment button
-//increment count on clicked button
-//
 
-// function incrementCount() {
-//  // count++;
-//  console.log("Button Clicked")
-//  //render(); 
-// }
-
-// function callout() {
-//   console.log(42)
-// }
-
-// callout();
-
-// let lap1 = 34;
-// let lap2 = 42;
-// let lap3 = 50;
-// function averageLapTime() {
-//   let sum = (lap1 + lap2 + lap3);
-//   console.log(sum);
-// }
-// averageLapTime();
-
+let countEl = document.getElementById("count-el");
 let count = 0;
+
 function increment() {
   count = count + 1;
+    countEl.innerHTML = count;
   console.log(count);
+}
+
+function save() {
+
 }
