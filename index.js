02bfc37655aef955333025f3c82705d560c8c060
Emmanuel <emmanueltar14@gmail.com>
2022-08-21 14:41:59 +0100

    pushing

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 let saveEl = document.getElementById('save-el');
  let countEl = document.getElementById("count-el");
- let count ;
-console.log(saveEl);
+ let count = 0;
+
 
  function increment() {
    count +=  1;
      countEl.innerHTML = count;
-   console.log(count);
+
 }
 
 function save() {
 let countStr = count + "-";
 saveEl.innerHTML += countStr;
-console.log(count);
  }
 save();
 
